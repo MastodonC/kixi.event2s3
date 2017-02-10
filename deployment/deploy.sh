a73ae1f4371f0f65883cb28106eafe5642090360
Jason Bell <jason@mastodonc.com>
2017-02-10 17:13:36 +0000

    Fix AWS_REGION in deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -9,10 +9,11 @@ TAG=$3
 ONYX_BATCH_SIZE=$4
 SYSTEM_PROFILE=production
 S3_BUCKET=$5
+AWS_REGION=eu-central-1
 
 # using deployment service sebastopol
 VPC=sandpit
-sed -e "s/@@TAG@@/$TAG/" -e "s/@@S3_BUCKET@@/$S3_BUCKET/" -e "s/@@ENVIRONMENT@@/$ENVIRONMENT/" -e "s/@@VPC@@/$VPC/" -e "s/@@CANARY@@/$CANARY/" -e "s/@@APP_NAME@@/$APP_NAME/" -e "s/@@IMAGE_PREFIX@@/$IMAGE_PREFIX/"  -e "s/@@INSTANCE_COUNT@@/$INSTANCE_COUNT/" -e "s/@@SYSTEM_PROFILE@@/$SYSTEM_PROFILE/" -e "s/@@ONYX_BATCH_SIZE@@/$ONYX_BATCH_SIZE/" ./deployment/marathon-config.json.template > $APP_NAME.json
+sed -e "s/@@TAG@@/$TAG/" -e "s/@@AWS_REGION/$AWS_REGION/" -e "s/@@S3_BUCKET@@/$S3_BUCKET/" -e "s/@@ENVIRONMENT@@/$ENVIRONMENT/" -e "s/@@VPC@@/$VPC/" -e "s/@@CANARY@@/$CANARY/" -e "s/@@APP_NAME@@/$APP_NAME/" -e "s/@@IMAGE_PREFIX@@/$IMAGE_PREFIX/"  -e "s/@@INSTANCE_COUNT@@/$INSTANCE_COUNT/" -e "s/@@SYSTEM_PROFILE@@/$SYSTEM_PROFILE/" -e "s/@@ONYX_BATCH_SIZE@@/$ONYX_BATCH_SIZE/" ./deployment/marathon-config.json.template > $APP_NAME.json
 
 # we want curl to output something we can use to indicate success/failure
 STATUS=$(curl -s -w "%{http_code}" -X POST http://$MASTER_URL/marathon/v2/apps/$APP_NAME -H "Content-Type: application/json" --data-binary "@$APP_NAME.json" | sed 's/.*\(...\)/\1/')
